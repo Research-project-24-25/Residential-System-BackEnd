f4921bad5d009a26ccc14048e8f6aeb824d6f78b
Rezdar Najeeb <125973154+RezdarNajeeb@users.noreply.github.com>
2025-03-08 23:22:53 +0300

    Add Resident model, controller, and migration for residents table

---
+++ app/Http/Controllers/ResidentController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Resident;
+use Illuminate\Http\Request;
+
+class ResidentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Resident $resident)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Resident $resident)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Resident $resident)
+    {
+        //
+    }
+}
