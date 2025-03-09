a38c23d1b705e53613c8f619fd1af17a708e9801
Rezdar Najeeb <125973154+RezdarNajeeb@users.noreply.github.com>
2025-03-10 01:00:52 +0300

    relationships are defined in House model.

--- app/Models/House.php
+++ app/Models/House.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class House extends Model
 {
     protected $guarded = ['id'];
+
+    protected $casts = [
+        'is_occupied' => 'boolean',
+        'number_of_residents' => 'integer',
+    ];
+
+    /**
+     * Get the residents living in this house
+     */
+    public function residents(): HasMany
+    {
+        return $this->hasMany(Resident::class);
+    }
 }
