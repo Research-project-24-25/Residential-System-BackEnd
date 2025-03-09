da228660684be681af4baab56306dee22cb36d90
Rezdar Najeeb <125973154+RezdarNajeeb@users.noreply.github.com>
2025-03-10 00:54:32 +0300

    HouseResource created.

---
+++ app/Http/Resources/HouseResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class HouseResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'house_number' => $this->house_number,
+            'number_of_residents' => $this->number_of_residents,
+            'house_type' => $this->house_type,
+            'is_occupied' => (bool) $this->is_occupied,
+            'residents' => ResidentResource::collection($this->whenLoaded('residents')),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
