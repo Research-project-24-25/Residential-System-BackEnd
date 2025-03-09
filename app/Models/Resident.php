73ad9c7cb643d5fd200af2bff0caa3232e919605
Rezdar Najeeb <125973154+RezdarNajeeb@users.noreply.github.com>
2025-03-10 00:15:07 +0300

    Updating Resident model for the authentication support.

--- app/Models/Resident.php
+++ app/Models/Resident.php
@@ -4,13 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class Resident extends Model
+class Resident extends Authenticatable
 {
+    use HasApiTokens, Notifiable;
+
     protected $guarded = ['id'];
+
+    protected $hidden = [
+        'password',
+    ];
 
     protected $casts = [
         'age' => 'integer',
+        'password' => 'hashed',
     ];
 
     /**
