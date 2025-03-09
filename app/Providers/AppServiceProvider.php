f0729f89731c6ed8f80011ef9a4dadfc3a3ef54d
Rezdar Najeeb <125973154+RezdarNajeeb@users.noreply.github.com>
2025-03-10 00:27:48 +0300

    Add admin authorization gates in the AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Admin;
 use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,7 +23,17 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
-            return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+            return config('app.frontend_url') . "/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+        });
+
+        // Define a super admin gate
+        Gate::define('super-admin', function (Admin $admin) {
+            return $admin->isSuperAdmin();
+        });
+
+        // Define admin gate
+        Gate::define('admin', function (Admin $admin) {
+            return $admin->isAdmin() || $admin->isSuperAdmin();
         });
     }
 }
