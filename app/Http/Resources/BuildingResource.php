480250971c33660df5f3c3c25e491b62209f44d5
Rezdar Najeeb <125973154+RezdarNajeeb@users.noreply.github.com>
2025-03-10 00:56:58 +0300

    BuildingResource created.

---
+++ app/Http/Resources/BuildingResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BuildingResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'address' => $this->address,
+            'total_floors' => $this->total_floors,
+            'floors' => FloorResource::collection($this->whenLoaded('floors')),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
