b96a854ff20160f5cc39e3e8a2f9bf8e9a7eef37
Rezdar Najeeb <125973154+RezdarNajeeb@users.noreply.github.com>
2025-03-10 00:16:19 +0300

    Password field added for residents migration.

--- database/migrations/2025_03_08_201547_create_residents_table.php
+++ database/migrations/2025_03_08_201547_create_residents_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')->unique();
+            $table->string('password');
             $table->string('phone_number');
             $table->integer('age');
             $table->enum('gender', ['male', 'female']);
