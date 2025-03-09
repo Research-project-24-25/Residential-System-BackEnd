3f4312cd5c0d8b977426a8257a195fa9b8384cff
Rezdar Najeeb <125973154+RezdarNajeeb@users.noreply.github.com>
2025-03-10 00:38:25 +0300

    ResidentResource created.

---
+++ app/Http/Resources/ResidentResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ResidentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'username' => $this->username,
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'email' => $this->email,
+            'phone_number' => $this->phone_number,
+            'age' => $this->age,
+            'gender' => $this->gender,
+            'status' => $this->status,
+            'house' => $this->when($this->house_id, new HouseResource($this->whenLoaded('house'))),
+            'apartment' => $this->when($this->apartment_id, new ApartmentResource($this->whenLoaded('apartment'))),
+            'created_by' => new AdminResource($this->whenLoaded('createdBy')),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
