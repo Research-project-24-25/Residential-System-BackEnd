28409be5ccfedde77625c995a3fadf7e13e4d0bf
Rezdar Najeeb <125973154+RezdarNajeeb@users.noreply.github.com>
2025-04-09 09:41:24 +0300

    Uses unique identifier for buildings
    Replaces separate name and address fields with a unique identifier.
    Enforces uniqueness and improves data integrity.

--- database/migrations/2025_02_17_192307_create_buildings_table.php
+++ database/migrations/2025_02_17_192307_create_buildings_table.php
@@ -13,8 +13,7 @@ return new class extends Migration
     {
         Schema::create('buildings', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('address');
+            $table->string('identifier')->unique(); // Unique identifier for the building within the compound
             $table->timestamps();
         });
     }
