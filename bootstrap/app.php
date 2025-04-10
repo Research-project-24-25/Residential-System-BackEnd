07960f065d155e587868dbe28f073524bf0778ff
Rezdar Najeeb <125973154+RezdarNajeeb@users.noreply.github.com>
2025-04-10 17:31:55 +0300

    Implements scheduled meeting reminders
    Adds a console command to send meeting reminders for upcoming meetings
    Registers the command to run daily at 10:00 via the scheduler

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Console\Scheduling\Schedule;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -25,4 +26,9 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })->withCommands(
+        //
+    )->withSchedule(function (Schedule $schedule) {
+        $schedule->command('meetings:send-reminders')->dailyAt('10:00');
+    })
+    ->create();
